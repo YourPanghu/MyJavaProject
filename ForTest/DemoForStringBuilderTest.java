aaf7cdd86b26cbfa4d8c43427d2059f408888f06
YourPanghu <1540817402@qq.com>
2024-07-12 13:59:28 +0800

    main

--- ForTest/DemoForStringBuilderTest.java
+++ ForTest/DemoForStringBuilderTest.java
@@ -5,10 +5,16 @@ class DemoForStringBuilderTest {
         //定义数组
         String[] data = {"迪丽热巴","古力娜扎","达拉崩吧","萨瓦迪卡"};
         // System.out.println(data);地址值
-        //读取数组内容
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
+        //输出数组内容，以[元素1，元素2，元素3...]的格式
+        System.out.print("[");
+		for(int i=0; i<data.length; i++){
+			if(i==data.length-1){
+				System.out.print(data[i] + "]");
+			}else{
+				System.out.print(data[i] + ",");
+			}
         }
+        System.out.println();
         //创建StringBuilder对象
         StringBuilder sb = new StringBuilder();
         //将元素拼接起来
@@ -16,5 +22,6 @@ class DemoForStringBuilderTest {
             sb.append(data[i]);
         }
         System.out.println(sb);
+        
     }
 }
