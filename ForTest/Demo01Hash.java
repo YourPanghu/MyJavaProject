bdf469c9a46865b95b6fddf21047004182d6e1bd
YourPanghu <1540817402@qq.com>
2024-08-20 12:15:31 +0800

    main

---
+++ ForTest/Demo01Hash.java
@@ -0,0 +1,10 @@
+package ForTest;
+
+public class Demo01Hash {
+    public static void main(String[] args){
+		Person p1 = new Person("韬哥",19);
+		Person p2 = new Person("韬哥",19);
+		System.out.println(p1);
+		System.out.println(p2);
+	}
+}
