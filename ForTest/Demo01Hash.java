364c22e11237377b200cbacb2f2da0b99576c01a
YourPanghu <1540817402@qq.com>
2024-08-20 13:25:38 +0800

    main

--- ForTest/Demo01Hash.java
+++ ForTest/Demo01Hash.java
@@ -6,5 +6,15 @@ public class Demo01Hash {
 		Person p2 = new Person("韬哥",19);
 		System.out.println(p1);
 		System.out.println(p2);
+
+		System.out.println(p1.hashCode());
+		System.out.println(p2.hashCode());
+
+		System.out.println("===========================");
+        
+        String s1 = "abc";
+        String s2 = new String("abc");
+        System.out.println(s1.hashCode());//96354
+        System.out.println(s2.hashCode());//96354
 	}
 }
